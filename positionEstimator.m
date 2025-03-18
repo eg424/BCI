4291a31ab6efe8e6b87976b8c865d23ed7d51843
Erik Garcia Oyono <eg424@ic.ac.uk>
2025-03-18 16:34:12 +0000

    Euler integration
    Added velocity components to predict hand position using Euler's to reduce the error

--- positionEstimator.m
+++ positionEstimator.m
@@ -1,10 +1,17 @@
 function [x, y] = positionEstimator(test_data, modelParameters)
     spikes = test_data.spikes;
     
-    % Compute mean firing rates as features
-    avg_firing_rates = mean(spikes, 2)';  % (1 x num_neurons)
-    
-    % Predict Hand Position using Linear Model
-    x = avg_firing_rates * modelParameters.Wx;
-    y = avg_firing_rates * modelParameters.Wy;
+    % Compute cum spike counts as features
+    cum_spikes = sum(spikes, 2)';
+
+    % Predict velocity
+    xdot = cum_spikes * modelParameters.Wx;
+    ydot = cum_spikes * modelParameters.Wy;
+
+    % Euler's integration to predict position
+    start_pos = test_data.startHandPos;
+    duration = size(spikes, 2);
+
+    x = start_pos(1) + xdot * duration;
+    y = start_pos(2) + ydot * duration;
 end
