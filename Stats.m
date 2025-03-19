dcb7d908f27faee3c619c3bae6b29f0870f283b1
Erik Garcia Oyono <eg424@ic.ac.uk>
2025-03-19 19:02:35 +0000

    Merge branch 'KalmanFilter' into SVMClassifier

--- Stats.m
+++ Stats.m
@@ -1,10 +1,19 @@
+%% Initial KF
+RMSE = 24.6087
+
+Total time taken: 20.1088 seconds
+Average time per prediction: 0.0030 seconds
+
+Weighted_rank = 24.1587
+
+%% SVM + KF
+% SVM Test
+SVM Classifier Accuracy: 90.00%
+Error Rate: 10.00%
+
+%SVM + KF
 >>testFunction_for_students_MTb
 RMSE = 25.0092
 Total time taken: 39.5983 seconds
 Average time per prediction: 0.0059 seconds
 Weighted_rank = 26.4681
-
-
->>test_SVM
-SVM Classifier Accuracy: 90.00%
-Error Rate: 10.00%
