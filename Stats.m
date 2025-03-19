f80c4bfaced25444957cba5ddfb69f2965284b47
Erik Garcia Oyono <eg424@ic.ac.uk>
2025-03-19 18:31:27 +0000

    Initial KF as final version
    Added stats for comparison of initial KF vs HSKF

--- Stats.m
+++ Stats.m
@@ -1,6 +1,15 @@
+%% Initial KF
 RMSE = 24.6087
 
 Total time taken: 20.1088 seconds
 Average time per prediction: 0.0030 seconds
 
 Weighted_rank = 24.1587
+
+%% HSKF
+RMSE = 25.0482
+
+Total time taken: 21.3906 seconds
+Average time per prediction: 0.0032 seconds
+
+Weighted_rank = 24.6824
