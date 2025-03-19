278451560acd55cfddc621713eedef66f8cb1402
Erik Garcia Oyono <eg424@ic.ac.uk>
2025-03-19 17:21:30 +0000

    Added pictures and stats.m
    Added pictures and stats file for comparison of initial model vs with Euler integration

---
+++ Stats.m
@@ -0,0 +1,16 @@
+%% Multivariate Linear Regression (LinRegInit.png)
+RMSE = 66.0110
+
+Total time taken: 4.4223 seconds
+Average time per prediction: 0.0007 seconds
+
+Weighted_rank = 59.8521
+
+
+%% MLR With Euler (LinRegEuler.png)
+RMSE = 28.3419
+
+Total time taken: 4.3697 seconds
+Average time per prediction: 0.0007 seconds
+
+Weighted_rank = 25.9446
