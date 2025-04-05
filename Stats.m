0402bb760cd5d09fea4b58cdfdf7ff0140bff2dd
Erik Garcia Oyono <eg424@ic.ac.uk>
2025-04-05 17:51:28 +0100

    Update Stats.m

--- Stats.m
+++ Stats.m
@@ -6,19 +6,8 @@ Average time per prediction: 0.0030 seconds
 
 Weighted_rank = 24.1587
 
+
 %% KalmanSVM
-% SVM Test
-SVM Classifier Accuracy: 90.00%
-Error Rate: 10.00%
-
-%SVM + KF
->>testFunction_for_students_MTb
-RMSE = 25.0092
-Total time taken: 39.5983 seconds
-Average time per prediction: 0.0059 seconds
-Weighted_rank = 26.4681
-
-%% KalmanSVM2
 % SVM Test
 SVM Classifier Accuracy: 92.25%
 Error Rate: 7.75%
@@ -30,3 +19,16 @@ Total time taken: 91.9412 seconds
 Average time per prediction: 0.0137 seconds
 
 Weighted_rank = 27.9204
+
+
+%% KalmanSVM2
+% SVM Test
+SVM Classifier Accuracy: 90.00%
+Error Rate: 10.00%
+
+%SVM + KF
+>>testFunction_for_students_MTb
+RMSE = 25.0092
+Total time taken: 39.5983 seconds
+Average time per prediction: 0.0059 seconds
+Weighted_rank = 26.4681
